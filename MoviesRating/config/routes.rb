637a7e784aa25ea3b7bb656ce1c4069d5476d684
Marcin Lis <marcin.lis07@gmail.com>
2017-03-19 22:53:29 +0100

    generate and download pdf added

--- MoviesRating/config/routes.rb
+++ MoviesRating/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  post '/rate' => 'rater#create', :as => 'rate'
   constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
     root to: "movies#index", as: :signed_in_root
   end
